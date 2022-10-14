d4953c0ea974c37b17e8ca8fc617bcc1f2186ab6
lucaspausin <lucaspausin@gmail.com>
2022-10-14 01:04:37 -0300

    Se agrega el Proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				darkcyan: "#3c8067",
+				cream: "#f2ebe3",
+				verydarkblue: "#1c232b",
+				darkgrayishblue: "#6c7289",
+				white: "hsl(0, 0%, 100%)",
+			},
+			fontFamily: {
+				monserrat: "Montserrat",
+				fraunces: "Fraunces",
+			},
+		},
+	},
+	plugins: [],
+};
